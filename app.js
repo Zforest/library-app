f9d6a570424d89c32db05b1600a476ec3847e4e1
Zforest <30865263+Zforest@users.noreply.github.com>
2023-07-11 14:51:18 -0400

    Built Framework for Book List and Adding New Books
    Created empty library book array
    Created book object constructor
    Created empty function that will be used to add new books to array

---
+++ app.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    function info() {
+        return `${title} by ${author}, ${pages} pages, ${read}.`;
+    }
+}
+
+function addBookToLibrary () {
+    // do stuff here
+}
